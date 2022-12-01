e4d6b473ad467d54486ed1b23ef341c04bc4e118
Muhammad Abutahir <61512822+Abutahir12@users.noreply.github.com>
2022-12-01 19:13:57 +0530

    initial commit for sqs (#9)
    Co-authored-by: abutahir <tahir@codeops.tech>

---
+++ serverless-sqs-tutorials/handler.py
@@ -0,0 +1,10 @@
+import json
+
+
+def push_to_q(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    return {"statusCode": 200, "body": json.dumps(body)}
